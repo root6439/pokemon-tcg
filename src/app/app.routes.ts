c42c0e3604c00e3b4568d9c292ba8936bccf399c
Nicolas Soares Henrique <nicolas.soaresh.98@gmail.com>
2024-04-16 08:50:35 -0300

    [feature] - configuração inicial

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { DecksComponent } from './pages/decks/decks.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: "", component: DecksComponent}
+];
---
+++ src/app/pages/decks/decks.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-decks',
+  standalone: true,
+  imports: [],
+  templateUrl: './decks.component.html',
+  styleUrl: './decks.component.scss'
+})
+export class DecksComponent {
+
+}
